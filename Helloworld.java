f4c7c3c0e85fd4d053f14a2616a18f9510721bdb
Aji Sathyanandhan <83906143+ajislal@users.noreply.github.com>
2024-10-06 14:37:31 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,4 +1,4 @@
-class HelloWorld {
+class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World! Testing"); 
         System.out.println("Hello, World! Testing1"); 
